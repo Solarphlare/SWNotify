4660615c6e28bcb5dc7a22f4ba8cbfed743c536b
William M. <cominatyou@cominatyou.com>
2024-12-15 23:34:22 -0600

    Set C language standard to C18

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SWNotify",
-            targets: ["SWNotify", "CNotify"]),
+            targets: ["SWNotify", "CNotify"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -24,5 +25,6 @@ let package = Package(
             dependencies: ["CNotify"]
         )
     ],
-    swiftLanguageModes: [.v5]
+    swiftLanguageModes: [.v5],
+    cLanguageStandard: .c18
 )
